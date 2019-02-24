183f2c33b7a25011919d131c75cd591dce32d44c
Sandy Taillan <tdk1@orange.fr>
2019-02-24 11:03:04 +0100

    Création de la page d'accueil avant de la rendre adaptatif

--- front-page.php
+++ front-page.php
@@ -1,6 +1,13 @@
+<!--todo : mettre le fonds avec des couleurs et un motif transparent
+    todo : Créer un index.php pour le fil actualités
+    todo : créer une page de sommaire pour le fil articles
+    todo : créer une page de contact
+    todo : créer une page à propos
+    todo : mettre en adaptatif : bootstrap
+
+-->
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
     <head> <!--Metadata -->
         <meta charset="<?php bloginfo('charset'); ?>" >
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,18 +18,33 @@
     </head>
     <body>
         <header>
-            <h1>Linux and CO</h1>
-            <p>Site sur Linux en particulier et sur l'informatique en général.</p>
-            <img src="" alt="">
+            <div id="header-entete">
+                <div id="header-texte">
+                    <h1>Linux and Co.</h1>
+                    <p>Un site sur Linux en particulier et sur l'informatique en général.</p>
+                </div>
+                <img src="/wp-content/themes/linux-and-co_wordpress/img/image-header-site.png" alt="image-site">
+            </div>
+            <nav id="header-nav">
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Articles</a></li>
+                    <li><a href="http://linux-and-co.dessins-plaisirs.fr/actualites">Actualités</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">À propos</a></li>
+                </ul>
+            </nav>
         </header>
         <main>
-
-            <p style="text-align: justify">"Linux and co" est un site pour parler de Linux et des logiciels fonctionnant sous la distribution Ubuntu, mais pas uniquement.<br>
-                En effet, J'aimerai vous aider à vous familiariser avec l'informatique et son vocabulaire si particulier. Et comme la programmation est indissociable de l'informatique, vous pourrez y trouver des lignes de codes. Mes langages de prédilection sont le Python et le Bash.<br><br>
-                En fait, "Linux and Co" contiendra tout ce qui me passionne dans le monde de l'informatique.</p>
-        </main>
+            <section id="section-frontpage">
+                <img src="/wp-content/themes/linux-and-co_wordpress/img/informatique_ecran-1.jpg" alt="image-ordinateur">
+                <p style="text-align: justify">"Linux and Co." est un site pour parler de Linux et des logiciels fonctionnant sous la distribution Ubuntu, mais pas uniquement.<br>
+                    En effet, J'aimerai vous aider à vous familiariser avec l'informatique et son vocabulaire si particulier. Et comme la programmation est indissociable de l'informatique, vous pourrez y trouver des lignes de codes. Mes langages de prédilection sont le Python et le Bash.<br><br>
+                    En fait, "Linux and Co" contiendra tout ce qui me passionne dans le monde de l'informatique.</p>
+            </section>
+           </main>
         <footer>
-
+            <p>Copyright © 2019 - Sandy Taillan</p>
         </footer>
     </body>
 </html>
